09aff5dd17188e43e15376d084f800dc2c7ad209
piggydoughnut <drmikhailova@gmail.com>
2020-11-09 17:00:08 +1300

    initial Gatsby setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `ChipsPoker`,
+    siteUrl: `https://chips.cash`,
+    description: `CHIPS poker`,
+  },
   plugins: [],
 }
