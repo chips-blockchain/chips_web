c9381bdb77d8d1e568e68da5d0139df46f47b076
piggydoughnut <drmikhailova@gmail.com>
2020-12-02 17:50:23 +1300

    added the benefit card molecule

---
+++ src/components/Molecules/BenefitCard.tsx
@@ -0,0 +1,44 @@
+import React, { ReactElement } from "react"
+import styled from '@emotion/styled'
+import Img, { FixedObject } from "gatsby-image"
+import { sampleBase64 } from "../../utils/sampleBase64"
+
+type BenefitCardProps = {
+    image: FixedObject,
+    title: string,
+    text: string
+}
+
+const defaultProps = {
+    image: sampleBase64,
+    title: "Lorem Ipsum",
+    /* 180 characters */
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est velit. Lobortis sceler."
+}
+
+const BenefitCardContainer = styled.div`
+    width: 300px;
+    padding: 1rem;
+
+    & > p, h1 {
+        color: var(--color-almostWhite);
+        margin-left: 2rem;
+    }
+
+    & > p {
+        font-size: var(--font-size-p);
+        font-family: var(--font--family-secondary);
+        line-height: var(--line-height-2);
+    }
+`
+
+const BenefitCard = ({ image, title, text }: BenefitCardProps): ReactElement => (
+    <BenefitCardContainer>
+        <Img style={{height: '300px', width: '300px'}} fixed={image}></Img>
+        <h1>{title}</h1>
+        <p>{text}</p>
+    </BenefitCardContainer>
+)
+
+BenefitCard.defaultProps = defaultProps
+export default BenefitCard
---
+++ src/utils/benefitsData.js
@@ -0,0 +1,22 @@
+export const benefitsData = [
+    {   
+        image: "benefitSecure",
+        title: "Secure",
+        text: "CHIPS is secured by <a href='#'>Komodo’s delayed proof-of-work</a> security mechanism. DPow is a revolutionary consensus mechanism which provides security and protection to many other currencies."
+    },
+    {
+        image: "benefitShortTime",
+        title: "Seamless betting",
+        text: "Bets are placed quickly and there is no time delay due the blockchain processing. Short blocktimes, paired with the Lightning Network implementation, decrease wait times for tables."
+    },
+    {
+        image: "benefitRealTime",
+        title: "Peer to peer",
+        text: ""
+    },
+    {
+        image: "benefitBtcTech",
+        title: "Powered by Bitcoin",
+        text: ""
+    }
+]
