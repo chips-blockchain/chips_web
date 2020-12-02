3e0a845c242166d8d4c0a7f75e52277b8cd244df
piggydoughnut <drmikhailova@gmail.com>
2020-12-02 15:31:16 +1300

    added default props and types for typescript

--- src/components/Molecules/Navigation.tsx
+++ src/components/Molecules/Navigation.tsx
@@ -1,21 +1,39 @@
-import React from "react"
+import React, { ReactElement } from "react"
 import NavigationLink from "../Atoms/NavigationLink"
+import styled from '@emotion/styled'
 
+type MenuLink = {
+    link: string,
+    name: string,
+}
 
-const Navigation = ({ menuLinks, textColor }) => (
-    <div>
+type NavigationProps = {
+    menuLinks: Array<MenuLink>,
+    textColor: string
+}
+
+const defaultProps = {
+    menuLinks: [{link: "/", name: "Home"}],
+    textColor: "#FFFFFF"
+}
+const NavigationContainer=styled.div`
+    padding-top: 1.5rem;
+`
+
+const Navigation = ({ menuLinks, textColor }: NavigationProps): ReactElement => (
+    <NavigationContainer>
         {menuLinks.map(link => (
             <NavigationLink
                 to={link.link}
                 key={link.name}
                 textColor={textColor}
                 activeClassName="active"
-                partiallyActive={link.partiallyActive}
             >
             {link.name}
         </NavigationLink>
         ))}
-    </div>
+    </NavigationContainer>
 )
 
+Navigation.defaultProps = defaultProps
 export default Navigation
