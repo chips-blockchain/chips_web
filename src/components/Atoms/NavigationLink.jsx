7eb228a887deb6bb5d21a55bfd9374a3f05d97ee
piggydoughnut <drmikhailova@gmail.com>
2020-12-02 15:10:43 +1300

    rename navigation link to jsx

--- src/components/Atoms/NavigationLink.jsx
+++ src/components/Atoms/NavigationLink.jsx
@@ -4,10 +4,10 @@ import styled from "@emotion/styled"
 const NavigationLink = styled(Link)`
   align-items: left;
   color: ${props => props.textColor};
-  font-weight: bold;
   justify-content: center;
-  padding: 0.5rem;
+  padding: 0.5rem 1rem 0.5rem;
   text-decoration: none;
+  font-size: var(--font-size-h2)
 `;
 
 export default NavigationLink;
