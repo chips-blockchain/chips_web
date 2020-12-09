d6667627cb3f2285d35911df0f4a9aec5e972bab
piggydoughnut <drmikhailova@gmail.com>
2020-12-09 18:06:12 +1300

    added specific font weights

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
 import "./src/styles/global.css"
-import "fontsource-montserrat"
-import "fontsource-rubik";
+import "fontsource-rubik/600-normal.css";
+import "fontsource-rubik/400-normal.css";
+import "fontsource-rubik/300-normal.css";
+import "fontsource-montserrat/600-normal.css";
+import "fontsource-montserrat/400-normal.css";
+import "fontsource-montserrat/300-normal.css";
