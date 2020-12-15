2c7d226d728c7c0cf406f057e829f3c94df388c5
piggydoughnut <drmikhailova@gmail.com>
2020-12-15 16:57:23 +1300

    added lorem helper

---
+++ src/utils/lorem.js
@@ -0,0 +1,5 @@
+const loremTitle = 'Lorem ipsum'
+const loremSubtitle = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+const loremText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est velit. Lobortis sceler."
+
+export { loremTitle, loremSubtitle, loremText}
