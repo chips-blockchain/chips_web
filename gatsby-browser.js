e9757727bd19ee9999e2ca04d9baea644fcf65f5
piggydoughnut <drmikhailova@gmail.com>
2020-12-18 15:09:19 +1300

    polyfill for smooth scrolling for safari

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,6 @@ import "fontsource-rubik/600-normal.css"
 import "fontsource-rubik/400-normal.css"
 import "fontsource-montserrat/600-normal.css"
 import "fontsource-montserrat/400-normal.css"
+// smooth scroll polyfill to make the scrolling smooth in Safari
+import smoothscroll from 'smoothscroll-polyfill';
+smoothscroll.polyfill();
