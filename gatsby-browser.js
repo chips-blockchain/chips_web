c74aca38cbf5faa962d0a925fbe090ee0098087f
piggydoughnut <drmikhailova@gmail.com>
2021-01-25 17:51:55 +1300

    added a new font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
 import "./src/styles/global.css"
-import "fontsource-rubik/600-normal.css"
-import "fontsource-rubik/400-normal.css"
-import "fontsource-montserrat/600-normal.css"
-import "fontsource-montserrat/400-normal.css"
+import "@fontsource/rubik/600.css"
+import "@fontsource/rubik/400.css"
+import "@fontsource/raleway/600.css"
+import "@fontsource/raleway/400.css"
 // smooth scroll polyfill to make the scrolling smooth in Safari
 import smoothscroll from 'smoothscroll-polyfill';
 smoothscroll.polyfill();
